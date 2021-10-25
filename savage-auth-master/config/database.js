8e716352c7251f504b42b7521831357dfd24c7b2
lTokko <jhomarcuevas03@gmail.com>
2021-10-24 23:35:09 -0400

    TasteForLater

---
+++ savage-auth-master/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://lTokkol:Jcuevas3@cluster0.9mnt6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+    'dbName': 'RestarauntTracker'
+};
